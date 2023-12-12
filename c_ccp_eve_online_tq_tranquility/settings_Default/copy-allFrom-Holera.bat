1307722f40afffcb94bb652bf95cf7dcae5444c3
Dmytro Cheva <dmytro.cheva@gmail.com>
2023-12-13 00:36:59 +0200

    Add New accounts Update copy-allFrom-Holera.bat

--- c_ccp_eve_online_tq_tranquility/settings_Default/copy-allFrom-Holera.bat
+++ c_ccp_eve_online_tq_tranquility/settings_Default/copy-allFrom-Holera.bat
@@ -6,9 +6,14 @@ copy core_public__.yaml "Worm Holera/" /Y
 copy core_char_2121853336.dat "Worm Holera/" /Y
 copy core_user_29280260.dat "Worm Holera/" /Y
 
+copy core_char_2121853336.dat core_char_578564394.dat /Y
 copy core_char_2121853336.dat core_char_2113875139.dat /Y
 copy core_char_2121853336.dat core_char_2114656127.dat /Y
+copy core_char_2121853336.dat core_char_2115007749.dat /Y
+copy core_char_2121853336.dat core_char_2115031890.dat /Y
 copy core_char_2121853336.dat core_char_2115457291.dat /Y
+copy core_char_2121853336.dat core_char_2115479508.dat /Y
+copy core_char_2121853336.dat core_char_2118248041.dat /Y
 copy core_char_2121853336.dat core_char_2119361891.dat /Y
 copy core_char_2121853336.dat core_char_2120347635.dat /Y
 copy core_char_2121853336.dat core_char_2121174545.dat /Y
@@ -19,11 +24,14 @@ copy core_char_2121853336.dat core_char_2121178171.dat /Y
 copy core_char_2121853336.dat core_char_2121178396.dat /Y
 copy core_char_2121853336.dat core_char_2121853336.dat /Y
 copy core_char_2121853336.dat core_char_2121878074.dat /Y
+copy core_char_2121853336.dat core_char_2121891348.dat /Y
 
 copy core_user_29280260.dat core_user_2110311.dat /Y
 copy core_user_29280260.dat core_user_16492377.dat /Y
 copy core_user_29280260.dat core_user_17525312.dat /Y
+copy core_user_29280260.dat core_user_18000247.dat /Y
 copy core_user_29280260.dat core_user_18034259.dat /Y
+copy core_user_29280260.dat core_user_18922386.dat /Y
 copy core_user_29280260.dat core_user_23622903.dat /Y
 copy core_user_29280260.dat core_user_25218835.dat /Y
 copy core_user_29280260.dat core_user_26239201.dat /Y
