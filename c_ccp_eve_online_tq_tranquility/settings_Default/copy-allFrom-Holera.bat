f8f75979d8200a397f338af537fdf3a7114d47bd
Dmytro Cheva <dmytro.cheva@gmail.com>
2023-12-21 23:18:16 +0200

    Close session

--- c_ccp_eve_online_tq_tranquility/settings_Default/copy-allFrom-Holera.bat
+++ c_ccp_eve_online_tq_tranquility/settings_Default/copy-allFrom-Holera.bat
@@ -1,11 +1,12 @@
 @echo off
+@echo Backup Worm Holera
 copy prefs.ini "Worm Holera/" /Y
 copy core_char__.dat "Worm Holera/" /Y
 copy core_user__.dat "Worm Holera/" /Y
 copy core_public__.yaml "Worm Holera/" /Y
 copy core_char_2121853336.dat "Worm Holera/" /Y
 copy core_user_29280260.dat "Worm Holera/" /Y
-
+@echo Copy characters
 copy core_char_2121853336.dat core_char_578564394.dat /Y
 copy core_char_2121853336.dat core_char_2113875139.dat /Y
 copy core_char_2121853336.dat core_char_2114656127.dat /Y
@@ -25,3 +26,16 @@ copy core_char_2121853336.dat core_char_2121178396.dat /Y
 copy core_char_2121853336.dat core_char_2121853336.dat /Y
 copy core_char_2121853336.dat core_char_2121878074.dat /Y
 copy core_char_2121853336.dat core_char_2121891348.dat /Y
+@echo Copy users 
+copy core_user_29280260.dat core_user_2110311.dat /Y
+copy core_user_29280260.dat core_user_16492377.dat /Y
+copy core_user_29280260.dat core_user_17525312.dat /Y
+copy core_user_29280260.dat core_user_18000247.dat /Y
+copy core_user_29280260.dat core_user_18034259.dat /Y
+copy core_user_29280260.dat core_user_18922386.dat /Y
+copy core_user_29280260.dat core_user_23622903.dat /Y
+copy core_user_29280260.dat core_user_25218835.dat /Y
+copy core_user_29280260.dat core_user_26239201.dat /Y
+copy core_user_29280260.dat core_user_28182799.dat /Y
+copy core_user_29280260.dat core_user_29280260.dat /Y
+copy core_user_29280260.dat core_user_29285645.dat /Y
